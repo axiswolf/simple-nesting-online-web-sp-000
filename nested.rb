783653c687a5376625b2740236687467ba1c2023
Karen V <karenvo@usc.edu>
2019-03-04 11:42:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -54,7 +54,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 return programmer_hash[:dennis_ritchie][:languages]
+		 programmer_hash[:dennis_ritchie][:languages]
 end
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -102,7 +102,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 puts programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
@@ -121,5 +121,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-print programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
