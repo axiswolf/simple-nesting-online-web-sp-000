780ed44b5f7c0d218451ceb52518903bb1e30569
Karen V <karenvo@usc.edu>
2019-03-04 11:18:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,7 +67,7 @@ def adding_matz
 			 :yukihiro_matsumoto => {
 			   	:known_for => "Ruby",
 			    :languages => ["LISP", "C"]
-			 }
+			 },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -81,8 +81,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-
 end
 
 def changing_alan
