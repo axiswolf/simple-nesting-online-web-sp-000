49a9f6bd1a5848d66d16a0fcd3bae942583d27d4
Karen V <karenvo@usc.edu>
2019-03-04 11:30:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-		 programmer_hash[:grace_hopper]
+		 puts programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -35,7 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-programmer_hash[:alan_kay][:known_for]
+puts programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -54,7 +54,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages]
+		 puts programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -103,7 +103,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 puts programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
@@ -122,5 +122,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 puts programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
